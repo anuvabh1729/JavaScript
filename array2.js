1459ca1dbfb5d962872d4869681026673197d7e4
anuvabh1729 <119966465+anuvabh1729@users.noreply.github.com>
2023-08-04 09:57:32 +0530

    Create array2.js

---
+++ array2.js
@@ -0,0 +1,10 @@
+//strings array
+let fruits = [ 'apple','banana','orange','grape', ];
+//print array
+console.log("The initial array is:",fruits );
+fruits.push("pineapple");
+// print after push operation
+console.log("The array after push:",fruits);
+fruits.pop(1);
+//print after pop operation
+console.log("The array after pop:",fruits);
