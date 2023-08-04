acd0a422bc93bbf324708da2bac8b68e351b8329
anuvabh1729 <119966465+anuvabh1729@users.noreply.github.com>
2023-08-04 09:57:05 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,9 @@
+let nos = [1,2,3,4,5,6,7];
+// print the initial array
+console.log("The intial array is:",nos);
+// length of the array
+console.log("The length of the array is:",nos.length);
+//print first number
+console.log("tThe first number is : ", nos[0]);
+//print last number
+console.log("The last number is : ", nos[nos.length-1]);
